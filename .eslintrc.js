4bc8394de79ec003bf1554b95d7fc4dfcfb85dad
Gerd Müller <gerdesque@posteo.org>
2022-04-18 17:42:34 +0200

    Ramp-up linting/prettier/ts/mui/storybook

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@next/next/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        printWidth: 70,
+      },
+    ],
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+import {
+  createTheme,
+  responsiveFontSizes,
+} from '@mui/material/styles'
+import { deepPurple, amber } from '@mui/material/colors'
+
+let theme = createTheme({
+  palette: {
+    primary: deepPurple,
+    secondary: amber,
+  },
+})
+
+theme = responsiveFontSizes(theme)
+
+export default theme
