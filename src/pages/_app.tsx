50a5878dd11a8decc82b7a462403c6882752c95f
Gerd Müller <gerdesque@posteo.org>
2022-05-15 11:53:45 +0200

    Add initial pages

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import theme from '../styles/theme'
+import createEmotionCache from '../styles/createEmotionCache'
+
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+export default function MyApp(props: MyAppProps) {
+  const {
+    Component,
+    emotionCache = clientSideEmotionCache,
+    pageProps,
+  } = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>PROTESTORY</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
