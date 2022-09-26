a5c07ced488ca936f58331315e1027f72e6fe2f9
Gerd Müller <gerdesque@posteo.org>
2022-09-26 16:01:06 +0200

    Adjust config items

--- src/config/startthedemo.tsx
+++ src/config/startthedemo.tsx
@@ -1,88 +1,201 @@
-import { Scene, Item } from '../context/data'
-
-export const scenes: Scene[] = [
+export const items = [
   {
-    id: 1,
-    name: 'start',
-    image: 'startthedemo/scenes/1/scene.webp',
-    clickables: [
-      {
-        id: 1,
-        image: 'startthedemo/scenes/1/clickables/icon.png',
-        left: 625,
-        top: 190,
-        message: 'Megafon',
-        scene: 2,
-        item: 1,
-      },
-      {
-        id: 2,
-        image: 'startthedemo/scenes/1/clickables/icon.png',
-        left: 500,
-        top: 500,
-        message: 'Lautsprecher und Mikro',
-        scene: 2,
-      },
-      {
-        id: 3,
-        image: 'startthedemo/scenes/1/clickables/icon.png',
-        left: 600,
-        top: 570,
-        message: 'Transporterschlüssel',
-        scene: 2,
-      },
-      {
-        id: 4,
-        image: 'startthedemo/scenes/1/clickables/icon.png',
-        left: 560,
-        top: 250,
-        message: 'Transparente',
-        scene: 2,
-      },
-      {
-        id: 5,
-        image: 'startthedemo/scenes/1/clickables/icon.png',
-        left: 310,
-        top: 440,
-        message: 'Demoanmeldung',
-        scene: 2,
-      },
-      {
-        id: 6,
-        image: 'startthedemo/scenes/1/clickables/icon.png',
-        left: 470,
-        top: 380,
-        message: 'Mailingliste für Demomobilisierung',
-        scene: 2,
-      },
-    ],
+    active: false,
+    link: '',
+    id: 'verpflegung',
+    name: 'Rucksack mit Essen, Trinken, Regenkleidung und Demoshirt',
+    source: 'startthedemo/verpflegung.svg',
+    description:
+      'Bei einer Demo kann viele dazwischen kommen – meist ist es das Wetter. Während ihr als Teilnehmer:in bei strömenden Regen vielleicht eher zu Hause bleiben würdet, geht das als Organisationsteam natürlich nicht. Regenkleidung, Essen und Trinken sind also ein Muss - wer weiß, wie lang der Tag noch wird. Und um euren Protest gegen den Tagebau-Ausbau auch an anderen Tagen öffentlich zu zeigen, habt ihr direkt auch noch T-Shirts aus Biobaumwolle mit dem Demoslogan drucken lassen.',
   },
   {
-    id: 2,
-    name: 'transparente',
-    image: 'startthedemo/scenes/2/scene.webp',
-    clickables: [
-      {
-        id: 2,
-        image: 'startthedemo/scenes/1/clickables/icon.png',
-        left: 100,
-        top: 400,
-        message: 'Transparente nah',
-        scene: 1,
-      },
-    ],
-  },
-]
-
-export const items: Item[] = [
-  {
-    id: 1,
-    name: 'key',
-    image: 'startthedemo/items/key.png',
+    active: false,
+    link: '',
+    id: 'schluessel',
+    name: 'Transporterschlüssel',
+    source: 'startthedemo/schluessel.svg',
+    description:
+      'Karla hat einen Transporter mit offener Ladefläche organisiert. Da ihr den Autoschlüssel gefunden habt, kann es nun auch losgehen.',
   },
   {
-    id: 2,
-    name: 'hammer',
-    image: 'startthedemo/items/hammer.png',
+    active: false,
+    link: 'revolutiontelephone',
+    id: 'festnetz',
+    name: 'Presse informieren (Festnetztelefon)',
+    source: 'startthedemo/festnetz.svg',
+    description:
+      'Du hast den Pressekontakt gefunden! Social Media hin oder her, aber viel Berichterstattung findet immer noch über normale Tageszeitungen und deren Portale statt. Insbesondere wenn ihr ältere und unbeteilgte Menschen von eurer Sache überzeugen wollt, solltet ihr auch klassische Medien kontaktieren, um eure Demo und euer Anliegen publik zu machen und zu erklären.',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'baseball',
+    name: 'Baseball',
+    source: 'startthedemo/baseball.svg',
+    leave: true,
+    description:
+      'Ihr wollt nach der Demo noch zum Baseball-Training? Klingt logisch für euch, aber wahrscheinlich nicht für die Polizei. Die wird darin eher eine potentielle Waffe sehen. Wenn ihr also keinen Anlass für Stress bieten wollt, lasst ihn lieber zu Hause.',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'namen',
+    name: 'Rednerliste',
+    source: 'startthedemo/namen.svg',
+    description:
+      'Keine Demo ohne Redebeiträge! Ohne Redebeiträge weiß man oft gar nicht, wofür oder wogegen demonstriert wird. Sie dienen dazu, der Demo eine klare Richtung zu geben und die eignenen Anliegen klar zu formulieren. Da alle Redner:innen sich ebenfalls auf ihre Beiträge vorbereiten müssen, solltet ihr früh anfangen, geeignete Menschen dafür zu finden und absprechen, wer worüber spricht.',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'demoshirt',
+    name: 'Demoshirt',
+    source: 'startthedemo/demoshirt.svg',
+    description:
+      'Bei einer Demo kann viele dazwischen kommen – meist ist es das Wetter. Während ihr als Teilnehmer:in bei strömenden Regen vielleicht eher zu Hause bleiben würdet, geht das als Organisationsteam natürlich nicht. Regenkleidung, Essen und Trinken sind also ein Muss - wer weiß, wie lang der Tag noch wird. Und um euren Protest gegen den Tagebau-Ausbau auch an anderen Tagen öffentlich zu zeigen, habt ihr direkt auch noch T-Shirts aus Biobaumwolle mit dem Demoslogan drucken lassen.',
+  },
+  {
+    active: false,
+    link: 'slogangenerator',
+    id: 'megafon',
+    name: 'Megafon',
+    source: 'startthedemo/megafon.svg',
+    description:
+      'Ein Megafon ist superwichtig, z.B. um Sprechchöre anzustimmen. Als Ergänzung zu Lautsprechern und Mikrofon ist es auch mobiler einsetzbar, sobald sich die Demonstration in Bewegung setzt. Und deine Stimme ist vielleicht nicht kräftig genug, um über einige Hundert Meter hinweg hörbar zu sein.',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'books_floor',
+    name: 'Bücherstapel',
+    source: 'startthedemo/books_floor.svg',
+    leave: true,
+    description:
+      'Eine Demo kann chillig sein, aber wahrscheinlich gitb es immer genug zu tun. Du wirst also eher keine Zeit haben, nebenbei noch was zu lesen. Also mach lieber deinen Rucksack leichter und lass die Bücher zu Hause.',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'smartphone',
+    name: 'Social Media für Demomobilisierung',
+    source: 'startthedemo/smartphone.svg',
+    description:
+      'Je jünger eure Zielgruppe für die Demo, desto wichtiger ist Social Media – das ist klar. Wenn ihr sehr früh mit der Demoplanung angefangen habt, ist es gut, bereits einige Wochen vorher den Termin bekannt zu machen, denn die wenigsten entscheiden sich sponatn an einer Demo teilzunehmen.',
+  },
+  {
+    active: false,
+    link: 'concert',
+    id: 'gitarre',
+    name: 'Gitarre für Live-Musik',
+    source: 'startthedemo/gitarre.svg',
+    description:
+      'Ihr habt Glück und einer eurer Freunde will live zwischen einigen Reden auftreten und zwei eigene Songs spielen. Dafür hat Karla eine Gitarre besorgt, die ihr an den Verstärker anschließen könnt. Vergesst den Soundcheck nicht!',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'hund',
+    name: 'Hund',
+    source: 'startthedemo/hund.svg',
+    leave: true,
+    description:
+      'Eine Demo ist für Tiere meist nicht der richtige Platz, denn es sind zu viele Menschen auf engstem Raum. Hunde macht das oft unruhig und manche Menschen haben Angst vor Hunden. Ihr tut ihm also einen Gefallen, wenn ihr ihn zu Hause lasst.',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'lautsprecher_mikro',
+    name: 'Lautsprecher und Mikro',
+    source: 'startthedemo/lautsprecher_mikro.svg',
+    description:
+      'Da ihr einen Transporter mit offener Ladefläche als Bühne nutzen wollt, ist es dort praktisch, ein Mikrofon mit Lautsprecher zu haben. Gerade eure Redner:innen wollen gut von allen gehört werden. Testet am Besten vorab, wie gut die Qualität und die Lautstärke der Anlage ist.',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'messer',
+    name: 'Messer',
+    source: 'startthedemo/messer.svg',
+    leave: true,
+    description:
+      'Auch wenn ihr nur Obst damit schneiden wollt, ist es besser, das Messer zu Hause zu lassen, da es auch als Waffe gelesen werden kann. Oft wird das Mitführen von Messer auch in den Demoauflagen der Polizei untersagt.',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'checklist',
+    name: 'Checklist für die Demo',
+    source: 'startthedemo/checklist.svg',
+    description:
+      'Karla und du habt zu Beginn eurer Planungen eine lange Checklist geschrieben, damit ihr auch nichts vergesst bei der Organisation. Dabei helfen euch auch bereits bestehende Checklists, wie z.B. diese von Aktion Mensch erstellte -> https://delivery-aktion-mensch.stylelabs.cloud/api/public/content/online-tipps-checklisten.pdf?v=639b5fef Schaut doch mal rein.',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'glasflasche',
+    name: 'Glasflasche',
+    source: 'startthedemo/glasflasche.svg',
+    leave: true,
+    description:
+      'Eine Glasflasche mitzunehmen, ist keine gute Idee. Oft wird dies bereits in den Demo-Auflagen der Polizei untersagt. Es besteht die Befürchtung, dass eine Glasflasche auch als Wurfgeschoss eingesetzt werden könnte und Menschen verletzt.',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'papierstapel',
+    name: '',
+    source: 'startthedemo/papierstapel.svg',
+    description: '',
+  },
+  {
+    active: false,
+    link: 'posterparade',
+    id: 'plakate',
+    name: 'Plakate für Demoaufruf',
+    source: 'startthedemo/plakate.svg',
+    description:
+      'Insta, Twitter, Mailinglists, alles schön und gut. Aber manche Leute werden auch einfach durch Plakate in ihrem Kiez auf eine Demo aufmerksam. Vergesst also nicht, auch im Analogen Werbung für eure Demo zu machen, damit sich möglichst verschiedene Menschen für euer Anliegen einsetzen und euch unterstützen können.',
+  },
+  {
+    active: false,
+    link: 'posterparade',
+    id: 'transparente',
+    name: 'Transparente',
+    source: 'startthedemo/transparente.svg',
+    description:
+      'Keine Demo kommt ohne Palakate, Parolen und Transparente aus. Für die erste Reihe habt ihr ein großes Banner drucken lassen, auf dem "Kohle stoppen, Klima und Dörfer retten!" steht.',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'laptop',
+    name: '',
+    source: 'startthedemo/laptop.svg',
+    description: '',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'poster',
+    name: '',
+    source: 'startthedemo/poster.svg',
+    description: '',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'adressen',
+    name: '',
+    source: 'startthedemo/adressen.svg',
+    description: '',
+  },
+  {
+    active: false,
+    link: '',
+    id: 'ordner_weste',
+    name: 'Kontakte zu Demo- Ordner:innen (gelbe Warnweste)',
+    source: 'startthedemo/ordner_weste.svg',
+    description:
+      'Diesen Job macht keiner gern und auf vielen Demos werden noch spontan Ordner:innen gesucht. Ihr habt zum Glück schon eine Liste mit Menschen, die sich freiwillig bereit erklärt haben, als Ordner:innen die Demo zu flankieren. Ordner:innen sollen vorallem die Demo-Auflagen durchsetzen, also z.B. eingreifen, wenn jemand aggressiv wird oder Gegenstände dabei hat, die untersagt sind.',
   },
 ]
