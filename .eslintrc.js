070bbf150bbcd4c769e06853ba088138859fd9a4
Gerd Müller <gerdesque@posteo.org>
2022-10-17 12:07:54 +0200

    Add export image optimization

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,35 +4,35 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@next/next/recommended",
-    "plugin:react/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@next/next/recommended',
+    'plugin:react/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint", "prettier"],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "prettier/prettier": [
-      "error",
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
         semi: false,
         printWidth: 70,
       },
     ],
-    "react/prop-types": "off"
+    'react/prop-types': 'off'
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 }
