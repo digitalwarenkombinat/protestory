e54054fb7124376a8a1ec383b198af702020d44b
Gerd Müller <gerdesque@posteo.org>
2022-11-20 14:04:19 +0100

    Handle media gallery portrait images

--- src/services/MediaDialog.tsx
+++ src/services/MediaDialog.tsx
@@ -2,23 +2,30 @@ import Dialog from '@mui/material/Dialog'
 import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export function MediaDialog({ onClose, selectedItem, open }) {
+  const [portrait, setPortrait] = useState(false)
   const handleClose = () => {
     onClose()
+  }
+
+  const handleImageLoad = (img: HTMLImageElement) => {
+    setPortrait(img.height > img.width)
   }
 
   return (
     <Dialog onClose={handleClose} aria-labelledby="dialog" open={open} maxWidth="md">
       <DialogTitle id="dialog-title">{selectedItem.name}</DialogTitle>
-      <DialogContent>
+      <DialogContent sx={{ textAlign: 'center' }}>
         <Image
+          onLoadingComplete={(img) => handleImageLoad(img)}
           src={selectedItem.source}
           alt={selectedItem.name}
           width={300}
           height={300}
           style={{
-            width: '100%',
+            width: portrait ? '70%' : '100%',
             height: 'auto',
           }}
         />
