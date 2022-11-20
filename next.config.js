5dadbbd840cd16f10b3d664c6db39b80a8694ad8
Gerd Müller <gerdesque@posteo.org>
2022-11-20 19:16:34 +0100

    Use preact in production

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 const withExportImages = require('next-export-optimize-images')
 
 module.exports = withExportImages({
-  // write your next.js configuration values.
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      }
+    }
+
+    return config
+  },
 })
