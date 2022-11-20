2affae6d65366dbca8e14762f2cb106916044ccd
Gerd Müller <gerdesque@posteo.org>
2022-11-20 17:56:20 +0100

    Remove empty spaces

--- src/config/mediaData.tsx
+++ src/config/mediaData.tsx
@@ -1,7 +1,7 @@
 export const mediaData = [
   {
     name: 'Foto einer Demonstration. Auf die Weltwirtschaftskrise in der Folge von 1929 reagierte die Regierung Heinrich Brüning mit drastischen Einsparungen im öffentlichen Bereich. Abgebildet ist der Schulstreik in Neukölln 1930. Auf dem Schild steht „Wegen Sparmaßnahmen des Magistrats streikt die 15. und 16. Gemeindeschule“. Magistrat nennt sich zu dieser Zeit die Spitze der städtischen Verwaltung. Die Kinder rufen zu Solidarität der Arbeitereltern auf. (AAJB Fotosammlung 02/1517)',
-    source: 'mediadata/02-1517_Demonstration 1930 Arbeitereltern unterstützt den Schulstreik in Neukölln.webp',
+    source: 'mediadata/02-1517_Demonstration_1930_Arbeitereltern.webp',
   },
   {
     name: 'Im Fokus dieses Fotos stehen zwei Frauen, welche gegen die Frauenarbeitslosigkeit im Dezember 1978 demonstrierten. (AAJB Fotosammlung 07_231)',
@@ -21,11 +21,11 @@ export const mediaData = [
   },
   {
     name: 'Dortmund 1976. Demonstration des Unterbezirk Dortmund der Sozialistischen Jugend Deutschlands die Falken. (AAJB Fotosammlung 07/0334)',
-    source: 'mediadata/7_334_SJD-Demo_Dortmund 1976_Wir fordern freie Jugendzentren_Kein Fotograf_Farbfoto.webp',
+    source: 'mediadata/7_334_SJD-Demo_Dortmund_1976.webp',
   },
   {
     name: 'Foto einer Friedensaktion der SJD-Die Falken in Köln, 1981. Jugendliche plakatieren an eine Werbetafel: Ein Tornado-Kampfjet entspricht nach Adam Riese zehn Schulen oder 51 Jugendheimen oder einer Viertel Million Fahrrädern. (AAJB Fotosammlung 08/0029)',
-    source: 'mediadata/8_29_SJD_Herne_1981_Ohne Fotograf_Kinder mit farbeimer.webp',
+    source: 'mediadata/8_29_SJD_Herne_1981.webp',
   },
   {
     name: 'Achtung Wortwitz! Der Baum ist sauer aufgrund des sauren Regens. Die DGB-Jugend agitiert hier für Bewusstsein gegenüber einem der wichtigsten Umweltthemen der 1980er Jahre: Das Waldsterben. (AAJB Sammlung Aufkleber 155)',
@@ -53,11 +53,11 @@ export const mediaData = [
   },
   {
     name: 'Flugblatt für ein Ferienlager, 1973. Das Zeltlagerinformationsblättchen zeigt ein Kind, dass glücklich im Stechschritt zur Sonne marschiert. Das Kinderlager borgt seinen Namen aus der ersten Strophe des wichtigsten Liedes der Arbeiter:innenjugendbewegung „Dem Morgenrot entgegen“. (AAJB Flugblattsammlung FL 592)',
-    source: 'mediadata/FL 592.webp',
+    source: 'mediadata/FL_592.webp',
   },
   {
     name: 'Veranstaltungsabzeichen der Roten Falken in Köln, 1957. Pfingsten war ein beliebter Termin für Zeltlager der Arbeiter:innenjugendbewegung. Das Motto auf dem Anstecker erinnert an das berühmte Lied „Arbeiter von Wien“. (AAJB Kleinformate 58)',
-    source: 'mediadata/Kleinformate 58 (Anstecknadel).webp',
+    source: 'mediadata/Kleinformate_58.webp',
   },
   {
     name: 'Dieser Anstecker, im Archiv der Arbeiterjugendbewegung mit ‚Kleinformate 233‘ signiert, plädiert „Nie wieder Nazis. Nie NPD.“ (AAJB Kleinformate 233)',
@@ -73,27 +73,27 @@ export const mediaData = [
   },
   {
     name: 'Rotes Megafon der Falken, ca. 1970. Das Megafon war häufig bei den Kundgebungen am 1. Mai dabei und vor allem bei der großen Demo nach der Räumung des AntiAtomDorfes „Republik freies Wendland“ in Hannover. (SJD – Die Falken, Bezirk Hannover Museales 51)',
-    source: 'mediadata/Museales 51.webp',
+    source: 'mediadata/Museales_51.webp',
   },
   {
     name: 'Diese aus dem Jahr 1982 stammende Postkarte findet sich unter der Signatur ‚PK 366‘. Die Postkarte kritisiert die Ausgabenpolitik in West-Berlin durch eine Karikatur und den Titel „Der Senat auf der Suche nach Geld für Spielplätze und Jugendzentren“. (AAJB Postkartensammlung PK 366)',
-    source: 'mediadata/PK 366.webp',
+    source: 'mediadata/PK_366.webp',
   },
   {
     name: 'Im Auftrag des Deutschen Bundesjugendrings erschien diese Postkarte im Jahr 1993. Unter der Signatur ‚PK 524‘ ist diese im Archiv der Arbeiterjugendbewegung zu finden. Gezeichnet wurde die Karte von Angela Salzburg und trägt den Titel „Plakatwettbewerb – Jugendverbände – gemeinsam was erleben“. (AAJB Postkartensammlung PK 524)',
-    source: 'mediadata/PK 524_1.webp',
+    source: 'mediadata/PK_524_1.webp',
   },
   {
     name: 'Diese Postkarte trägt die Signatur ‚PK 69‘, ist in Sandershausen erschienen und wird auf das Jahr 1947 datiert. Damals konnte sie für einen Preis von 50 Pfennig als Erlös für die Falken erworben werden. Zu sehen ist ein grafisch dargestelltes Zeltlager sowie die erste Strophe des Liedes „Die Arbeiter von Wien“. (AAJB SJD-Die Falken, Bezirk Hessen-Nord PK 69)',
-    source: 'mediadata/PK 69.webp',
+    source: 'mediadata/PK_69.webp',
   },
   {
     name: 'Unter der Signatur ‚PK 793‘ ist diese Postkarte zu finden. Sie wurde als Begleitmaterial zur Kampagne: „Wir Falken. Dein Widerstand.“ im Jahr 2017 veröffentlicht und trägt den Titel „Mädchenbande statt Burschenschaft.“ In Auftrag gegeben wurde sie von dem Bundesvorstand Berlin SJD - Die Falken. (AAJB SJD-Die Falken, Bundesvorstand PK 793)',
-    source: 'mediadata/PK 793_1.webp',
+    source: 'mediadata/PK_793_1.webp',
   },
   {
     name: 'Diese Postkarte mit der Signatur ‚PK 814‘ ist eine Luftballonkarte vom Kinderfest am 4.10.1986 des Ortsverbandes München-Ramersdorf. In Auftrag gegeben von den SJD – Die Falken, trägt sie den Titel „Ein Feriendorf für die Kinder Nicaraguas! Solidaritätsprojekt der SJD – Die Falken.“ (AAJB Die Falken, Ortsverband München PK 944)',
-    source: 'mediadata/PK 814_1.webp',
+    source: 'mediadata/PK_814_1.webp',
   },
   {
     name: 'Postkarte der Roten Falken Österreich. Zwei Kinder, die ein Transparent mit der Aufschrift „Vorwärts“ halten. Datierung unbekannt, wahrscheinlich 1980er Jahre. (AAJB Rote Falken Österreich PK 944)',
@@ -101,19 +101,19 @@ export const mediaData = [
   },
   {
     name: 'Plakat der Jusos, 1984. Seit den 1970er Jahren forderten die deutschen Gewerkschaften im Zusammenhang mit der Humanisierung der Arbeitswelt Arbeitszeitverkürzungen. Dies mündete in besonders erbittert geführten Streiks der IG Metall 1984. Die Forderung wird hier mit der Entwicklung der Arbeitslosenzahlen gegengeschnitten. (AAJB Plakatsammlung PL-A 188)',
-    source: 'mediadata/PL-A 188.webp',
+    source: 'mediadata/PL-A_188.webp',
   },
   {
     name: 'Plakat für eine Kulturveranstaltung am 17.11.1978 in Dortmund. Eingeladen sind neben dem deutschen Journalisten Günter Wallraff die beiden portugiesischen Liedermacher Jose Alfonso und Fausto Vitorino, deren Namen eng mit der Nelkenrevolution in Portugal 1974 verknüpft sind. Das Motiv des Plakates nutzt das Symbolbild der Revolution, eine rote Nelke im Gewehrlauf – allerdings hängt diese nun welk herab. Grandola, Vila Morena war die herausragende Hymne der Ereignisse von 1974. (AAJB Plakatsammlung PL-C 1123)',
-    source: 'mediadata/PL-C 1123.webp',
+    source: 'mediadata/PL-C_1123.webp',
   },
   {
     name: 'Von den Jungsozialist:innen in der SPD, Landesverband Baden-Württemberg, in Auftrag gegeben, erschien dieses Plakat anlässlich der Landtagswahl in Baden-Württemberg im Jahr 1980 mit der Aufforderung nach Gleichberechtigung. (AAJB Plakatsammlung PL-C 1202)',
-    source: 'mediadata/PL-C 1202_zuklein.webp',
+    source: 'mediadata/PL-C_1202_zuklein.webp',
   },
   {
     name: 'Plakat eines sozialen Bündnisses aus politischen Gruppen und Gewerkschaften, wahrscheinlich 1985. Auf dem Plakat sind unterschiedliche soziale Gruppen repräsentiert, die für die Themen Umwelt, Arbeiter:innen-Interessen und Abrüstung auf die Straße gehen. Agitiert wird für eine Abendveranstaltung mit Kulturprogramm und eine Demonstration des DGB. (AAJB Plakatsammlung PL-C 1401)',
-    source: 'mediadata/PL-C 1401.webp',
+    source: 'mediadata/PL-C_1401.webp',
   },
   {
     name: 'Das Plakat wurde anlässlich des Aktionstages gegen Rechts am 29.1.2000 in Wuppertal von der SJD – Die Falken, Bezirk Niederrhein, in Auftrag gegeben. Es zeigt eine rote Falkenflagge und darunter ein Hakenkreuz, das von einem Stock zerschlagen wird. (AAJB Plakatsammlung PL-B 126)',
