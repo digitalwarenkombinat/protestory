1f10fb3388e93a844ac161d00c96b16cb832f8f4
Gerd Müller <gerdesque@posteo.org>
2022-11-27 22:24:42 +0100

    Add cookie dialog text config

---
+++ src/config/cookie.tsx
@@ -0,0 +1,32 @@
+export const cookieDialogText = {
+  de: {
+    mainDialogTitle: 'Social Media Inhalte',
+    mainDialogDescription:
+      'Wir nutzen Cookies, um dir Medieninhalte Dritter anzeigen zu können. Wir verarbeiten die Daten nur, wenn du uns durch Klicken auf "Zustimmen und Weiter" deine Einwilligung gibst oder über den Button "Einstellungen" eine spezifische Auswahl festlegst.',
+    mainDialogOptions: 'Einstellungen',
+    mainDialogAccept: 'Zustimmen und Weiter',
+    optionsDialogTitle: 'Deine Einstellungen zu Cookies für diese Website',
+    optionsDialogDescriptionAbove: 'Wir nutzen Cookies, um dir Medieninhalte Dritter anzeigen zu können. Triff hier deine persönliche Präferenz:',
+    optionsDialogSave: 'Auswahl speichern',
+    optionsDialogAccept: 'Alle akzeptieren',
+    descriptionTwitter: 'Inhalte von Twitter anzeigen.',
+    descriptionInstagram: 'Inhalte von Instagram anzeigen.',
+    descriptionYoutube: 'Inhalte von Youtube anzeigen.',
+    descriptionFacebook: 'Inhalte von Facebook anzeigen.',
+  },
+  en: {
+    mainDialogTitle: 'EN Social Media Inhalte',
+    mainDialogDescription:
+      'EN Wir nutzen Cookies, um dir Medieninhalte Dritter anzeigen zu können. Wir verarbeiten die Daten nur, wenn du uns durch Klicken auf "Zustimmen und Weiter" deine Einwilligung gibst oder über den Button "Einstellungen" eine spezifische Auswahl festlegst.',
+    mainDialogOptions: 'EN Einstellungen',
+    mainDialogAccept: 'EN Zustimmen und Weiter',
+    optionsDialogTitle: 'EN Deine Einstellungen zu Cookies für diese Website',
+    optionsDialogDescriptionAbove: 'EN Wir nutzen Cookies, um dir Medieninhalte Dritter anzeigen zu können. Triff hier deine persönliche Präferenz:',
+    optionsDialogSave: 'EN Auswahl speichern',
+    optionsDialogAccept: 'EN Alle akzeptieren',
+    descriptionTwitter: 'EN Inhalte von Twitter anzeigen.',
+    descriptionInstagram: 'EN Inhalte von Instagram anzeigen.',
+    descriptionYoutube: 'EN Inhalte von Youtube anzeigen.',
+    descriptionFacebook: 'EN Inhalte von Facebook anzeigen.',
+  },
+}
