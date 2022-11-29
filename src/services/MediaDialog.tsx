3c6356b3f1b126584537fecbef96fade572f22ab
Gerd Müller <gerdesque@posteo.org>
2022-11-29 14:55:41 +0100

    Adjust image orientation after load

--- src/services/MediaDialog.tsx
+++ src/services/MediaDialog.tsx
@@ -1,6 +1,7 @@
 import Dialog from '@mui/material/Dialog'
 import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
+import { useState } from 'react'
 
 import useStore from 'utils/store'
 import { useHasHydrated } from 'utils/useHasHydrated'
@@ -8,6 +9,12 @@ import { useHasHydrated } from 'utils/useHasHydrated'
 export function MediaDialog({ onClose, selectedItem, open }) {
   const hasHydrated = useHasHydrated()
   const { language } = useStore()
+  const [portrait, setPortrait] = useState(false)
+
+  const handleImageLoad = (img: HTMLImageElement) => {
+    setPortrait(img.height > img.width)
+  }
+
   const handleClose = () => {
     onClose()
   }
@@ -17,12 +24,13 @@ export function MediaDialog({ onClose, selectedItem, open }) {
       <DialogTitle id="dialog-title">{hasHydrated && selectedItem.name[language]}</DialogTitle>
       <DialogContent sx={{ textAlign: 'center' }}>
         <img
+          onLoad={(event) => handleImageLoad(event.target as HTMLImageElement)}
           src={selectedItem.source}
           alt={hasHydrated ? selectedItem.name[language] : 'Dialog'}
           width={300}
           height={300}
           style={{
-            width: '100%',
+            width: portrait ? '70%' : '100%',
             height: 'auto',
           }}
         />
