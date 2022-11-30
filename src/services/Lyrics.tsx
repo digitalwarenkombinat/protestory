bdaafdfde79a6badd885b603771975fc6e20e072
Gerd Müller <gerdesque@posteo.org>
2022-11-30 13:38:16 +0100

    Adjust concert videos for ios usage

--- src/services/Lyrics.tsx
+++ src/services/Lyrics.tsx
@@ -1,10 +1,11 @@
-import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardMedia from '@mui/material/CardMedia'
 import Grid from '@mui/material/Grid'
-import NoSsr from '@mui/material/NoSsr'
 import Typography from '@mui/material/Typography'
 import { useEffect, useState } from 'react'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { useTheme } from '@mui/material/styles'
 import { YouTubeEmbed } from 'react-social-media-embed'
 
 import { socialWallText } from 'config'
@@ -17,6 +18,8 @@ export const Lyrics = ({ activeElement }) => {
   const { acceptedCategories, language } = useStore()
   const [cookieDialogVisible, setCookieDialogVisible] = useState(acceptedCategories.length === 0)
   const [youtubeAccepted, setYoutubeAccepted] = useState(false)
+  const theme = useTheme()
+  const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'lg'))
 
   function setSocialMediaAcceptance() {
     setYoutubeAccepted(acceptedCategories.some((category) => category['key'] === 'youtube'))
@@ -45,21 +48,41 @@ export const Lyrics = ({ activeElement }) => {
         {activeElement?.link ? (
           <>
             <CookieDialog visible={cookieDialogVisible} onAccept={handleAccept} />
-            {!cookieDialogVisible && (
-              <NoSsr defer>
-                {youtubeAccepted ? (
-                  <YouTubeEmbed url={activeElement.link} height={455} />
-                ) : (
-                  <>
-                    <Typography component="h3" variant="h4" color="text.secondary" sx={{ my: 2 }}>
-                      {hasHydrated && socialWallText[language].description3}
-                    </Typography>
-                    <Button variant="outlined" sx={{ color: 'accent.main' }} onClick={() => setCookieDialogVisible(true)}>
-                      <Typography variant="body1">{hasHydrated && socialWallText[language].settings}</Typography>
-                    </Button>
-                  </>
-                )}
-              </NoSsr>
+            {!cookieDialogVisible && youtubeAccepted ? (
+              <Box sx={{ position: 'fixed' }}>
+                <style>{`
+                  .youtubeContainer {
+                    position: relative;
+                  }
+                `}</style>
+                <YouTubeEmbed url={activeElement.link} height={isTablet ? 300 : 455} width={isTablet ? 400 : 650} youTubeProps={{ containerClassName: 'youtubeContainer' }} />
+              </Box>
+            ) : (
+              <>
+                <Typography component="h3" variant="h4" color="text.secondary" sx={{ my: 2 }}>
+                  {hasHydrated && socialWallText[language].description3}
+                </Typography>
+                <Box
+                  sx={{
+                    cursor: 'pointer',
+                    color: 'accent.main',
+                    backgroundColor: 'transparent',
+                    border: '1px solid rgba(245, 0, 87, 0.5)',
+                    lineHeight: '1.75',
+                    textTransform: 'uppercase',
+                    minWidth: '64px',
+                    padding: '5px 15px',
+                    '&:hover': {
+                      textDecoration: 'none',
+                      backgroundColor: 'rgba(245, 0, 87, 0.04)',
+                      border: '1px solid #f50057',
+                    },
+                  }}
+                  onClick={() => setCookieDialogVisible(true)}
+                >
+                  <Typography variant="body1">{hasHydrated && socialWallText[language].settings}</Typography>
+                </Box>
+              </>
             )}
           </>
         ) : (
--- src/services/Timeline.tsx
+++ src/services/Timeline.tsx
@@ -1,47 +1,53 @@
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 
 import { concert } from 'config/concert'
 
 export const Timeline = ({ setActiveElement }) => (
-  <Box
-    sx={{
-      position: 'relative',
-      display: 'flex',
-      backgroundColor: 'text.secondary',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderBottom: '8px solid black',
-      overflow: 'auto',
-      height: '106.04px',
-      borderTopLeftRadius: '79.33px',
-      borderTopRightRadius: '79.33px',
-      paddingTop: '8px',
-      px: '8px',
-    }}
-  >
-    {concert.timeline.map(({ index, year }) => (
-      <Button
-        key={index}
-        onClick={() => setActiveElement(concert.timeline[index])}
-        sx={{
-          color: 'text.primary',
-          margin: 0.5,
-          textTransform: 'none',
-          borderRight: '2px solid black',
-          borderRadius: 0,
-          boxShadow: 'none',
-          backgroundColor: 'transparent',
-          '&:last-child': {
-            borderRight: 'none',
-          },
-        }}
-      >
-        <Typography component="p" variant="h4">
-          {year}
-        </Typography>
-      </Button>
-    ))}
-  </Box>
+  <Grid item xs={12}>
+    <Card
+      sx={{
+        display: 'flex',
+        backgroundColor: 'text.secondary',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottom: '8px solid black',
+        overflow: 'auto',
+        height: '106.04px',
+        borderTopLeftRadius: '79.33px',
+        borderTopRightRadius: '79.33px',
+        paddingTop: '8px',
+        px: '8px',
+      }}
+    >
+      <Grid container alignItems="center" justifyContent="center" textAlign="center">
+        {concert.timeline.map(({ index, year }) => (
+          <Box
+            key={index}
+            onClick={() => setActiveElement(concert.timeline[index])}
+            sx={{
+              cursor: 'pointer',
+              padding: '6px 8px',
+              color: 'text.primary',
+              margin: 0.5,
+              textTransform: 'none',
+              borderRight: '2px solid black',
+              borderRadius: 0,
+              boxShadow: 'none',
+              backgroundColor: 'transparent',
+              '&:last-child': {
+                borderRight: 'none',
+              },
+            }}
+          >
+            <Typography component="p" variant="h4">
+              {year}
+            </Typography>
+          </Box>
+        ))}
+      </Grid>
+    </Card>
+  </Grid>
 )
